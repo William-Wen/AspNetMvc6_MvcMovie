eb56f37f947a58ed5274296cea4412de947a2cd0
william_wen <william_wen@wistron.com>
2016-02-23 06:12:29 +0800

    Adding a controller http://docs.asp.net/projects/mvc/en/latest/getting-started/first-mvc-app/adding-controller.html Add the routing of the parameters.

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is William's default action...";
+        }
+        
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes = 1)
+        {
+      //return "This is the Welcome action method...";
+      return HtmlEncoder.Default.HtmlEncode(
+        "Hello" + name + ", NumTimes is: " + numTimes);
+        }
+
+    }
+}
