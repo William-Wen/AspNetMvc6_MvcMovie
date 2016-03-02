451c48b1df464b857862a056f3c5939ff9dc42e2
William Wen <william.wen@gmail.com>
2016-03-02 09:00:27 +0800

    Finished this chapter.

--- src/MvcMovie/Controllers/HelloWorldController.cs
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -11,19 +11,29 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
-        public string Index()
+        //// GET: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is William's default action...";
+        //}
+        public IActionResult Index()
         {
-            return "This is William's default action...";
+            return View();
         }
 
-        // GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
-        {
-            //return "This is the Welcome action method...";
-            return HtmlEncoder.Default.HtmlEncode(
-              "Hello " + name + ", ID: " + ID);
-        }
+        //// GET: /HelloWorld/Welcome/
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    //return "This is the Welcome action method...";
+        //    return HtmlEncoder.Default.HtmlEncode(
+        //      "Hello " + name + ", ID: " + ID);
+        //}
 
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
     }
 }
